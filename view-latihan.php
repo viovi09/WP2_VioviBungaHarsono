e12b0d16a122a42b3d066a84e77b955453885b34
viovi09 <vistudies16@gmail.com>
2024-04-30 12:46:09 +0700

    menambahkan view-data-matakuliah.php,view-form-matakuliah.php,view-latihan.php

---
+++ view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    <p>Nilai 2 = <?= $nilai2; ?></p>
+    jadi, hasil dari pemodelan dengan metode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
